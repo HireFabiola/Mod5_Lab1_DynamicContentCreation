b5a6ef5ea4f4649d5da07264a3af3cf0b2021f9a
Fabiola <fabiola.aurelien@gmail.com>
2026-03-27 14:51:00 -0400

    Items added and updated totals accordingly

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const productNameInput = document.getElementById('product-name');
 const productPriceInput = document.getElementById('product-price');
 const addProductButton = document.getElementById('add-product');
+const removeItemButton = document.getElementById('remove-product');
+
 const cart = document.getElementById('cart');
 const totalPriceSpan = document.getElementById('total-price');
 
@@ -8,11 +10,6 @@ let totalPrice = 0;
 
 addProductButton.addEventListener('click', handleAddButton);
 
-// Function to update the total price
-function updateTotalPrice(amount) {
-    totalPrice += amount;
-    totalPriceSpan.textContent = totalPrice.toFixed(2);
-}
 
 // Function to remove an item
 function removeItem(event) {
@@ -24,12 +21,31 @@ function removeItem(event) {
 
 // Function to add product 
 function handleAddButton(event) {
-    console.log("I made it here");
+
     const productName = productNameInput.value;
-    const productPrice = productPriceInput.value;
-    console.log(productPrice);
+    const productPrice = parseFloat(productPriceInput.value);
+
     console.log(productName);
+    console.log(productPrice);g
+
+    // Check to ensure neither field is empty
+    if ((productName === "") || (productPrice === "")) {
+        alert("You must enter a product AND a price");
+        return;
+    }
+
+    // Update list with product name and price
     const li = document.createElement("li")
-    li.textContent = productName + " $" + productPrice;
-    cart.appendChild(li);
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    cart.append(productName, productPrice, removeButton);
+
+    updateTotalPrice(productPrice);
 }
+
+// Function to update the total price
+function updateTotalPrice(amount) {
+    totalPrice += amount;
+    totalPriceSpan.textContent = totalPrice.toFixed(2);
+}
+
