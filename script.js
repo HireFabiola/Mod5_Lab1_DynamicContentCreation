200104e9a78a4e97da2fab592790597763b5c4a9
Fabiola <fabiola.aurelien@gmail.com>
2026-03-27 15:42:42 -0400

    Correctly remove item upon click of remove button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const productNameInput = document.getElementById('product-name');
 const productPriceInput = document.getElementById('product-price');
 const addProductButton = document.getElementById('add-product');
-const removeItemButton = document.getElementById('remove-product');
+// const removeItemButton = document.getElementById('remove-product');
 
 const cart = document.getElementById('cart');
 const totalPriceSpan = document.getElementById('total-price');
@@ -11,22 +11,24 @@ let totalPrice = 0;
 addProductButton.addEventListener('click', handleAddButton);
 
 
+
 // Function to remove an item
 function removeItem(event) {
+    console.log("Am I here?");
     const item = event.target.closest('li');
-    const price = parseFloat(item.dataset.price);
-    updateTotalPrice(-price);
+    console.log(item);
+    // const price = parseFloat(item.dataset.price);
+    // updateTotalPrice(-price);
     item.remove();
 }
 
 // Function to add product 
 function handleAddButton(event) {
-
     const productName = productNameInput.value;
     const productPrice = parseFloat(productPriceInput.value);
 
     console.log(productName);
-    console.log(productPrice);g
+    console.log(productPrice);
 
     // Check to ensure neither field is empty
     if ((productName === "") || (productPrice === "")) {
@@ -38,8 +40,13 @@ function handleAddButton(event) {
     const li = document.createElement("li")
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
-    cart.append(productName, productPrice, removeButton);
+    removeButton.addEventListener('click', removeItem);
 
+    li.append(productName + "  $" + productPrice);
+    li.appendChild(removeButton);
+
+    cart.appendChild(li);
+    console.log(cart);
     updateTotalPrice(productPrice);
 }
 
